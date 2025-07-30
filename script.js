a4bbab55c97e0d510b75f58ed17a4f16539fa3dd
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-07-30 10:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
 function functionName() {
-	alert("functionName")
-	
+  alert(arguments.callee.name);
 }
+
 function AccioJob() {
-	alert("AccioJob");
-	
+  alert(arguments.callee.name);
 }
-functionName()
-AccioJob()
+
+functionName();  // Alerts: "functionName"
+AccioJob(); 
